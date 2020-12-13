59d005627a6c135863a850b210084cbe1f40cf79
Andres Navarro <andresnavag93@gmail.com>
2020-12-13 13:12:39 -0400

    :hammer: Adding Redis microservice

--- config.js
+++ config.js
@@ -12,16 +12,22 @@ module.exports = {
     secret: process.env.JWT_SECRET || 'notasecret!',
   },
   mysql: {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DB,
-  },
-  mysqlService: {
-    port: process.env.MYSQL_SRV_PORT || 3001,
+    host: process.env.MYSQL_HOST || '',
+    user: process.env.MYSQL_USER || 'vyU7DCqzKX',
+    password: process.env.MYSQL_PASS || 'hQ8gtxIQ9o',
+    database: process.env.MYSQL_DB || 'vyU7DCqzKX',
   },
   mysqlService: {
     host: process.env.MYSQL_SRV_HOST || 'localhost',
     port: process.env.MYSQL_SRV_PORT || 3001,
+  },
+  cacheService: {
+    host: process.env.MYSQL_SRV_HOST || 'localhost',
+    port: process.env.MYSQL_SRV_PORT || 3003,
+  },
+  redis: {
+    host: process.env.REDIS_HOST || 'redis-13556.c14.us-east-1-3.ec2.cloud.redislabs.com',
+    port: process.env.REDIS_PORT || 13556,
+    password: process.env.REDIS_PASS || '9sWIItAvlPbkoTcwOXstNm9hDFv7AU2F',
   },
 };
